d50f215ea4725723d87422d8ebe44a940b3f231e
antonio-bravo <79697559+antonio-bravo@users.noreply.github.com>
2021-12-01 20:07:30 +0100

    Create Function

--- DevOps_OnPrem/Migrate_Repo_AzureDevOps_GitHub.ps1
+++ DevOps_OnPrem/Migrate_Repo_AzureDevOps_GitHub.ps1
@@ -1,65 +1,163 @@
-$AzDOPAT = "<PAT Azure DevOps>"
-$AzDOOrg = "<Organization>"
-$AzDOPrj = "<AzureDevOps Project Name>" 
-$AzDORepo = "<Azure DevOps RepoName>"
+function Copy-ADO2GitHub {
+    <#
+    .SYNOPSIS
+        Function Copy repository from Azure DevOps to GitHub
 
-#GitHub information
-$GHPAT = "<PAT GitHub>"
-$GHUser = "<GitHub-Organization>"
-$GHRepo = "<Target Repo in GitHub that you have created previously>"
+    .DESCRIPTION
+        The function will copy repository from AzureDevOps to GitHub all branches and tags
 
-Write-Verbose "STARTING MIRROR MIGRATION FROM $AzDOOrg/$AzDOPrj/$AzDORepo to $GHUser/$GHRepo"
+    .PARAMETER ADOPAT
+        Azure DevOps PAT (Personal Access Token).
 
-# STEP 1: Make sure you have a local copy of all "old repo", branches and tags
-Write-Verbose 'Cloning Source Repo...'
+    .PARAMETER ADOOrg
+        Azure DevOps organization organization.visualstudio.com
 
-if ( -not (Test-Path -LiteralPath '/migration' -PathType Container) )
-{
-    # This means we didn't mount an external volume
-    Set-Location /
+    .PARAMETER APOPrj
+        Azure DevOps project
 
-    mkdir migration
+    .PARAMETER ADORepo
+        Azure DevOps Repository
+
+    .PARAMETER GHPAT
+        GitHub PAT (Personal Access Token).
+
+    .PARAMETER GHUser
+        GitHub User or Organization
+
+    .PARAMETER GHRepo
+        GitHub Target Repository
+
+    .EXAMPLE
+        Copy-ADO2GitHub -ADOPAT "<Azure DevOps PAT>" -ADOOrg "<Azure DevOps Organization>" -APOPrj "<Azure DevOps Project>" -ADORepo "<Azure DevOps Repostory>" -GHPAT "<GitHub PAT>" -GHUser "<GitHub User or Organization>" -GHRepo "<GitHub Target Project"
+
+        Copy reposotory from Azure DevOps to GitHub
+    #>
+
+    [CmdletBinding(SupportsShouldProcess)]
+
+    Param(
+    [Parameter( Mandatory = $true)]
+    $ADOPAT, 
+    [Parameter( Mandatory = $true)]
+    $ADOOrg,
+    [Parameter( Mandatory = $true)]
+    $ADOPrj,
+    [Parameter( Mandatory = $true)]
+    $ADORepo,
+    [Parameter( Mandatory = $true)]
+    $GHPAT,
+    [Parameter( Mandatory = $true)]
+    $GHUser,
+    [Parameter( Mandatory = $true)]
+    $GHRepo
+    )
+
+
+    begin {
+        # Check parameters
+        if (-not $ADOPAT) {
+            Stop-PSFFunction -Message "Please enter Azure DevOps PAT" -Target $ADOPAT
+            return
+        }
+
+        if (-not $ADOOrg) {
+            Stop-PSFFunction -Message "Please enter a Azure DevOps Organization" -Target $ADOOrg
+            return
+        }
+        
+        if (-not $ADOPrj) {
+            Stop-PSFFunction -Message "Please enter a Azure DevOps Project" -Target $ADOPrj
+            return
+        }
+
+        if (-not $ADORepo) {
+            Stop-PSFFunction -Message "Please enter a Azure DevOps Repository" -Target $ADORepo
+            return
+        }
+
+        if (-not $GHPAT) {
+            Stop-PSFFunction -Message "Please enter a GitHub PAT" -Target $GHPAT
+            return
+        }
+
+        if (-not $GHUser) {
+            Stop-PSFFunction -Message "Please enter a GitHub User or Organization" -Target $GHUser
+            return
+        }
+
+        if (-not $GHRepo) {
+            Stop-PSFFunction -Message "Please enter a GitHub Target Repository" -Target $GHRepo
+            return
+        }
+
+
+    }
+
+    process {
+            # $ADOPAT = "<PAT Azure DevOps>"
+            # $ADOOrg = "<Organization>"
+            # $ADOPrj = "<AzureDevOps Project Name>" 
+            # $ADORepo = "<Azure DevOps RepoName>"
+
+            # #GitHub information
+            # $GHPAT = "<PAT GitHub>"
+            # $GHUser = "<GitHub-Organization>"
+            # $GHRepo = "<Target Repo in GitHub that you have created previously>"
+
+            Write-Verbose "STARTING MIRROR MIGRATION FROM $ADOOrg/$ADOPrj/$ADORepo to $GHUser/$GHRepo"
+
+            # STEP 1: Make sure you have a local copy of all "old repo", branches and tags
+            Write-Verbose 'Cloning Source Repo...'
+
+            if ( -not (Test-Path -LiteralPath '/migration' -PathType Container) )
+            {
+                # This means we didn't mount an external volume
+                Set-Location /
+
+                mkdir migration
+            }
+
+            Set-Location migration
+
+            $migrationFolder = "$ADORepo-Migration"
+
+            mkdir $migrationFolder
+
+            Set-Location $migrationFolder
+
+            $cloneUrl = https://$ADOPAT@$ADOOrg.visualstudio.com/$ADOPrj/_git/$ADORepo
+
+            git clone --mirror $cloneUrl .
+
+            # STEP 2: Add a "new repo" as a new remote origin:
+
+            Write-Verbose 'Adding Target Repo as Origin'
+
+            $GHoriginUrl = https://github.com/$GHUser/$GHRepo.git
+                        
+
+            git remote add GHorigin $GHoriginUrl
+
+            # STEP 3: Push everything to the new repo.
+
+            Write-Verbose 'Pushing all code, branches, tags, and history to Target Repo...'
+
+            $GHpushUrl = https://$GHPAT@github.com/$GHUser/$GHRepo.git 
+                    
+            git push --mirror $GHpushUrl
+
+            # STEP 4. Clean up. Remove "old repo" origin and its dependencies, and rename new origin
+
+            Write-Verbose 'Cleaning up...'
+
+            git remote -v
+
+            git remote rm origin
+
+            git remote rename GHorigin origin
+
+            Write-Verbose 'MIGRATION COMPLETED'
+            ### Done! Now your local git repo is connected to "new repo" remote
+            ### which has all the branches, tags and commits history.
+    }
 }
-
-Set-Location migration
-
-$migrationFolder = "$AzDORepo-Migration"
-
-mkdir $migrationFolder
-
-Set-Location $migrationFolder
-
-$cloneUrl = https://$AzDOPAT@$AzDOOrg.visualstudio.com/$AzDOPrj/_git/$AzDORepo
-
-git clone --mirror $cloneUrl .
-
-# STEP 2: Add a "new repo" as a new remote origin:
-
-Write-Verbose 'Adding Target Repo as Origin'
-
-$GHoriginUrl = https://github.com/$GHUser/$GHRepo.git
-            
-
-git remote add GHorigin $GHoriginUrl
-
-# STEP 3: Push everything to the new repo.
-
-Write-Verbose 'Pushing all code, branches, tags, and history to Target Repo...'
-
-$GHpushUrl = https://$GHPAT@github.com/$GHUser/$GHRepo.git 
-           
-git push --mirror $GHpushUrl
-
-# STEP 4. Clean up. Remove "old repo" origin and its dependencies, and rename new origin
-
-Write-Verbose 'Cleaning up...'
-
-git remote -v
-
-git remote rm origin
-
-git remote rename GHorigin origin
-
-Write-Verbose 'MIGRATION COMPLETED'
-### Done! Now your local git repo is connected to "new repo" remote
-### which has all the branches, tags and commits history.
